ddce2098849bf0300a251b58e117c0335880dd71
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-07-11 02:50:48 -0700

    Basic Mesh Export (Pos / Tris only), submeshes not merged
    I still need to make submeshes children of parent meshes & figure out how to do .mtl

--- src/relic/model/archive/mesh.py
+++ src/relic/model/archive/mesh.py
@@ -1,9 +1,10 @@
 import json
+import os
 import struct
 from dataclasses import dataclass
-from io import BytesIO
-from os.path import join, splitext
-from typing import BinaryIO, List
+from io import BytesIO, FileIO
+from os.path import join, splitext, dirname
+from typing import BinaryIO, List, TextIO
 
 from relic.model.archive import chunky
 
@@ -51,7 +52,7 @@ class MsgrChunk:
 
     @classmethod
     def create(cls, chunk: FolderChunk) -> 'MsgrChunk':
-        data = get_chunk_by_id(chunk.chunks, "DATA",flat=True)
+        data = get_chunk_by_id(chunk.chunks, "DATA", flat=True)
         with BytesIO(data.data) as stream:
             buffer = stream.read(_NUM.size)
             count = _NUM.unpack(buffer)[0]
@@ -114,13 +115,21 @@ class MslcChunk:
     V_SIZE = 48
     I_SIZE = 2
 
+    # name:str
+
     header: MsclHeader
     names: List[MslcName]
+
     unk_b: int
+
     vertex_data: bytes
+
     unk_c: int
+
     textures: List[str]
+
     index_data: bytes
+
     unk_d: int
     unk_e: int
     unk_f: int
@@ -128,6 +137,7 @@ class MslcChunk:
 
     @classmethod
     def create(cls, chunk: FolderChunk) -> 'MslcChunk':
+        # name = chunk.name
         data = get_chunk_by_id(chunk.chunks, "DATA")
 
         with BytesIO(data.data) as stream:
@@ -168,12 +178,85 @@ def raw_dump():
     chunky.dump_all_chunky(r"D:\Dumps\DOW I\sga", r"D:\Dumps\DOW I\whm-chunky", [".whm"])
 
 
-if __name__ == "__main__":
-    with open(r"D:\Dumps\DOW I\sga\art\ebps\races\chaos\troops\aspiring_champion.whm", "rb") as handle:
+def print_meta(f: str):
+    with open(f, "rb") as handle:
         chunky = RelicChunky.unpack(handle)
         whm = WhmChunk.create(chunky)
         meta = json.dumps(whm, indent=4, cls=EnhancedJSONEncoder)
         print(meta)
+
+
+def dump_obj(f: str, o: str):
+    def write_vertex(stream: TextIO, x, y, z):
+        stream.write('v %f %f %f\n' % (x, y, z))
+
+    def write_tri(stream: TextIO, *args):
+        stream.write('f')
+        for v in args:
+            stream.write(' %i' % v)
+        stream.write("\n")
+
+    try:
+        os.makedirs(dirname(o))
+    except FileExistsError:
+        pass
+
+    with open(o + ".obj", "w") as obj:
+        with open(f + ".meta") as m:
+            meta = json.loads(m.read())
+
+        with open(f + ".vert","rb") as v:
+            _POS = struct.Struct("< f f f")
+            for _ in range(int(meta['vertexes'])):
+                buffer = v.read(_POS.size)
+                x, y, z = _POS.unpack(buffer)
+                write_vertex(obj, x, y, z)
+
+        with open(f + ".tri","rb") as t:
+            _TRI = struct.Struct("< h h h")
+            for _ in range(int(meta['triangles'])):
+                buffer = t.read(_TRI.size)
+                a, b, c = _TRI.unpack(buffer)
+                write_tri(obj, a + 1, b + 1, c + 1) # blender is 1th based NOT 0th based
+
+
+def dump_all_obj(f:str):
+    for root, file in walk_ext(f,".vert"):
+        full = join(root, file)
+        full, _ = splitext(full)
+        dump_obj(full,full)
+
+
+def dump_model(f: str, o: str):
+    print("" + f)
+    with open(f, "rb") as handle:
+        chunky = RelicChunky.unpack(handle)
+        whm = WhmChunk.create(chunky)
+
+        for i, mesh in enumerate(whm.msgr.submeshes):
+            name = whm.msgr.parts[i].name
+            full_o = join(o, name)
+            try:
+                os.makedirs(dirname(full_o))
+            except FileExistsError:
+                pass
+            print("\t" + full_o)
+
+            with open(full_o + ".vert", "wb") as v:
+                v.write(mesh.vertex_data)
+            with open(full_o + ".tri", "wb") as t:
+                t.write(mesh.index_data)
+            with open(full_o + ".meta", "w") as m:
+                m.write(json.dumps({'vertexes': len(mesh.vertex_data) / 48, 'triangles': len(mesh.index_data) / 6}))
+
+
+if __name__ == "__main__":
+    # print_meta(r"D:\Dumps\DOW I\sga\art\ebps\races\chaos\troops\aspiring_champion.whm")
+    # dump_model(r"D:\Dumps\DOW I\sga\art\ebps\races\chaos\troops\aspiring_champion.whm",
+    #            r"D:\Dumps\DOW I\whm-model\art\ebps\races\chaos\troops\aspiring_champion\\")
+    dump_all_obj(r"D:\Dumps\DOW I\whm-model\art\ebps\races\chaos\troops\aspiring_champion")
+    # dump_obj(r"D:\Dumps\DOW I\whm-model\art\ebps\races\chaos\troops\aspiring_champion\aspiring_champion_banner",
+    #            r"D:\Dumps\DOW I\whm-model\art\ebps\races\chaos\troops\aspiring_champion\aspiring_champion_banner")
 
 # 2a50 ~ 51 = 2a00 (VERTEX ONLY)
 # 10752 bytes OR 2688 4byte words
@@ -181,6 +264,8 @@ if __name__ == "__main__":
 # 0xEA ~ 234 ~ 45.9 OR 11.48 (prob no this)
 # 0xE0 ~ 224 ~ 48 OR 12
 # 0x27 ~ 39 ~  275.69 OR 68.92
+
+# 224 vertexes
 
 
 # Index Count is less obvious
@@ -228,3 +313,11 @@ if __name__ == "__main__":
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # WITH NO EVIDENCE WHATSOEVER I AM 100% CONFIDENT THAT BVOL is 'Bounding Volume'
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# VERTEX LAYOUT ( V representing vertex count ) ~ layout in bytes
+# POS: V * 3 (xyz) * 4 bytes (int32/float32?) ~ 12 ~ 0x258
+# NORM: V * 4 (???) * 4 bytes (int32/float32?) ~ 16 ~ 0x320
+# ???: V * 3 (???) * 4 bytes (int32/float32) ~ 12 ~ 0x258
+# UV: V * 2 (???) * 4 bytes    ~ 8 ~ 0x190         # 0x960-0x578 =0x3e8
+
+# 0x960-0x7d0?
