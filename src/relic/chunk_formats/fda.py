a5356721afed5c3f6de5a371f1f0bb172ef05a3b
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-07-15 17:42:32 -0700

    Dropped  aifc2fda & fda2aifc
    Probably shouldn't have included those in the first place, non-vital and my dumps didn't work properly (or I wasn't using it right).

--- src/relic/chunk_formats/fda.py
+++ src/relic/chunk_formats/fda.py
@@ -1,14 +1,10 @@
 # Stolen from https://scratchpad.fandom.com/wiki/Relic_Chunky_files
-import dataclasses
-import json
 import math
 import os
-import shutil
 import struct
-import tempfile
 from dataclasses import dataclass
 from io import BytesIO
-from os.path import join, dirname, splitext, exists, basename
+from os.path import join, splitext
 from typing import BinaryIO, List
 
 from relic.chunky.data_chunk import DataChunk
@@ -17,7 +13,6 @@ from relic.chunky.relic_chunky import RelicChunky
 from relic.chunky.relic_chunky_header import RelicChunkyHeader
 from relic.file_formats import aiff
 from relic.shared import walk_ext
-
 
 _INFO_STRUCT = struct.Struct("< L L L L L L L")
 _DATA_STRUCT = struct.Struct("< L")
@@ -87,7 +82,8 @@ class Converter:
             assert frames == int(frames)
             frames = int(frames)
 
-            aiff.write_COMM(temp, info.channels, frames, info.sample_size, info.sample_rate, cls.COMP, cls.COMP_desc, use_fixed=True)
+            aiff.write_COMM(temp, info.channels, frames, info.sample_size, info.sample_rate, cls.COMP, cls.COMP_desc,
+                            use_fixed=True)
             aiff.write_SSND(temp, chunky.data_block.data, info.block_bitrate)
             with BytesIO() as marker:
                 aiff.write_default_markers(marker)
@@ -119,100 +115,6 @@ class Converter:
         return FdaChunky(RelicChunkyHeader.default(), info, FdaDataChunk(len(data), data))
 
 
-def run_old():
-    class EnhancedJSONEncoder(json.JSONEncoder):
-        def default(self, o):
-            if dataclasses.is_dataclass(o):
-                return dataclasses.asdict(o)
-            if isinstance(o, bytes):
-                l = len(o)
-                if len(o) > 16:
-                    o = o[0:16]
-                    return o.hex(sep=" ") + f" ... [+{l - 16} Bytes]"
-                return o.hex(sep=" ")
-            return super().default(o)
-
-    in_root = "gen/sga/dump"
-    out_root_meta = "gen/fda/meta"
-    out_root_dump = "gen/fda/dump"
-    out_root_shared = "gen/fda/shared_dump"
-    for root, _, files in os.walk(in_root):
-        for file in files:
-            if ".fda" not in file:
-                continue
-
-            full_in = join(root, file)
-            full_out_meta = full_in.replace(in_root, out_root_meta)
-            full_out_dump = full_in.replace(in_root, out_root_dump)
-            print(full_in)
-            with open(full_in, "rb") as handle:
-                try:
-                    chunky = RelicChunky.unpack(handle)
-                except ValueError as e:
-                    print(f"\tNot Chunky?!\n\t\t'{e}'")
-                    continue
-
-            print("\tWriting Meta...")
-            meta = json.dumps(chunky, indent=4, cls=EnhancedJSONEncoder)
-            print("\t\t", meta)
-            meta_name = full_out_meta + ".json"
-            try:
-                os.makedirs(dirname(meta_name))
-            except FileExistsError:
-                pass
-            with open(meta_name, "w") as writer:
-                writer.write(meta)
-
-            print("\tWriting Assets...")
-            for name, c in chunky.walk_data():
-                print(f"\t\t{name}")
-                dump_name = join(full_out_dump, name)
-                print(f"\t\t\t{dump_name}")
-                try:
-                    os.makedirs(dirname(dump_name))
-                except FileExistsError:
-                    pass
-                with open(dump_name, "wb") as writer:
-                    writer.write(c.data_block)
-
-                shared_dump_name = join(out_root_shared, name)
-                try:
-                    os.makedirs(dirname(shared_dump_name))
-                except FileExistsError:
-                    pass
-                with open(shared_dump_name, "wb") as writer:
-                    writer.write(c.data_block)
-
-
-def run_new():
-    in_root = "gen/sga/dump"
-    out_root_dump = "gen/fda/dump"
-    for root, _, files in os.walk(in_root):
-        for file in files:
-            if ".fda" not in file:
-                continue
-
-            full_in = join(root, file)
-            print(full_in)
-            with open(full_in, "rb") as handle:
-                try:
-                    chunky = RelicChunky.unpack(handle)
-                except ValueError as e:
-                    print(f"\tNot Chunky?!\n\t\t'{e}'")
-                    continue
-                fda_chunky = FdaChunky.create(chunky)
-
-                full_out_dump = full_in.replace(in_root, out_root_dump)
-                full_out_dump = splitext(full_out_dump)[0] + ".aifc"
-                try:
-                    os.makedirs(dirname(full_out_dump))
-                except FileExistsError:
-                    pass
-
-                print("\tWriting Assets...")
-                print(f"\t\t{full_out_dump}")
-                with open(full_out_dump, "wb") as writer:
-                    Converter.Fda2Aiffr(fda_chunky, writer)
 
 
 def shared_dump(file: str, name: str, out_dir: str = None):
@@ -235,31 +137,6 @@ def shared_dump(file: str, name: str, out_dir: str = None):
             Converter.Fda2Aiffr(fda, writer, use_fixed=True)
 
 
-def safe_dump(file: str, name: str, out_dir: str = None):
-    out_dir = out_dir or "gen/safe_fda/shared_dump"
-    full = join(out_dir, name)
-    path = "../../dll/fda2aifc.exe"
-    path = os.path.abspath(path)
-    try:
-        os.makedirs(dirname(full))
-    except FileExistsError:
-        pass
-
-    import subprocess
-
-    def create_temporary_copy(path) -> str:
-        temp_dir = tempfile.gettempdir()
-        temp_path = os.path.join(temp_dir, basename(path))
-        shutil.copy2(path, temp_path)
-        return temp_path
-
-    temp_src = create_temporary_copy(file)
-    temp_dst = temp_src + ".aifc"
-    subprocess.call([path, f"{temp_src}", f"{temp_dst}"])
-    if exists(temp_dst):
-        shutil.copy2(temp_dst, full)
-
-
 def dump_all_fda(folder: str, out_dir: str = None, blacklist: List[str] = None, verbose: bool = False):
     blacklist = blacklist or []
     for root, file in walk_ext(folder, ".fda"):
@@ -279,27 +156,6 @@ def dump_all_fda(folder: str, out_dir: str = None, blacklist: List[str] = None, 
         f, _ = splitext(name)
 
         shared_dump(full, f + ".aifc", out_dir)
-
-
-def safe_dump_all_fda(folder: str, out_dir: str = None, blacklist: List[str] = None, verbose: bool = False):
-    blacklist = blacklist or []
-    for root, file in walk_ext(folder, ".fda"):
-        full = join(root, file)
-
-        skip = False
-        for word in blacklist:
-            if word in full:
-                skip = True
-                break
-
-        if skip:
-            continue
-        if verbose:
-            print(full)
-        name = full.lstrip(folder).lstrip("\\").lstrip("/")
-        f, _ = splitext(name)
-
-        safe_dump(full, f + ".aifc", out_dir)
 
 
 if __name__ == "__main__":
